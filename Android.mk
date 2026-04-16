d05d4034c5fffcff86449a8b7cb55c15eb9a3c78
nhansp <nhan.094.vn@gmail.com>
2026-04-16 09:26:48 +0000

    felix: Import blobs & firmware from BP4A.260205.001

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),felix)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,75ca02425854ef42763b7f2ed83c4b8ce2202a1b)
+$(call add-radio-file-sha1-checked,radio/bl1.img,4df268570da54669619fa1973c62029fba409505)
+$(call add-radio-file-sha1-checked,radio/bl2.img,ee41fd81e288fedf174a08ed43f5c009c2e448a8)
+$(call add-radio-file-sha1-checked,radio/bl31.img,6e3e620637a7456fb35a0fb52c1d11b409c05718)
+$(call add-radio-file-sha1-checked,radio/gsa.img,d5e1f409085dcff5b0e015b7214a6be79c10d16e)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,76635bc33a0513ebd5de4d48ae2908c76c9e99a4)
+$(call add-radio-file-sha1-checked,radio/modem.img,92207f66f5b1cdbc8530c800d704425349d7a2aa)
+$(call add-radio-file-sha1-checked,radio/pbl.img,c97d710a432c099e48a67796a3710f0bbfa75541)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,27fd462747a44af976645ef7d7df4a16c9a1e2e3)
+
+endif
